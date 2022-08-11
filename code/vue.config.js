170f5d017bdf10118bf00bf749536cbf3faa65ee
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-08-11 09:57:41 +0200

    working on configuration

--- code/vue.config.js
+++ code/vue.config.js
@@ -2,9 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   productionSourceMap: false,
-  devServer: {
+  devServer: 
+  {
     host: 'kanban-frontend-ke7hp.ondigitalocean.app',
-    port: 8080
   },
   parallel:true
 })
