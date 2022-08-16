73653bcf2afa79d8583b1065709b4a635b04cd26
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-08-16 17:24:19 +0200

    work enviroment

--- scripts/ldebug.sh
+++ scripts/ldebug.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/bash
+## Setup of the work enviroments so that the necesarry dependencies 
+## are build for debugging the system.
 
--- scripts/ldeploy.sh
+++ scripts/ldeploy.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-
+## Scripts used to make the system production ready for deployment to the cloud.
