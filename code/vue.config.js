bfea382510f5f7c8186979cc697c4caeb9dd995c
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-08-16 16:55:29 +0200

    remade code

--- code/vue.config.js
+++ code/vue.config.js
@@ -1,16 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  productionSourceMap: false,
-  
-  devServer: 
-  {
-    allowedHosts: "all",
-    client: {
-      reconnect: 4
-    },
-    compress: true,
-  },
-
-  parallel: true
+  transpileDependencies: true
 })
